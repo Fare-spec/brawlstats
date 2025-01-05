4623f88eb0f36cd37d330b1f0a703488c7a984ea
Spectre <elouan.fare@protonmail.ch>
2025-01-05 19:47:51 +0100

    Urgent auto commit

---
+++ src/fusion/mod.rs
@@ -0,0 +1,40 @@
+use serde_json::{Map, Value};
+use std::fs::File;
+use std::io::{self, BufReader, Write};
+
+
+fn read_json_object(file_path: &str) -> io::Result<Map<String, Value>> {
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+    let json_value: Value = serde_json::from_reader(reader)?; 
+
+    if let Value::Object(map) = json_value {
+        Ok(map) 
+    } else {
+        Err(io::Error::new(io::ErrorKind::InvalidData, "This JSON file isn't an object"))
+    }
+}
+
+fn merge_json_objects(file_paths: &[&str], output_file: &str) -> io::Result<()> {
+    let mut merged_map = Map::new();
+
+    for &file_path in file_paths {
+        let json_map = read_json_object(file_path)?;
+        for (key, value) in json_map {
+            merged_map.entry(key).or_insert(value);
+        }
+    }
+
+    let merged_json = Value::Object(merged_map);
+    let merged_json_str = serde_json::to_string_pretty(&merged_json)?;
+
+    let mut output = File::create(output_file)?;
+    writeln!(output, "{}", merged_json_str)?;
+
+    Ok(())
+}
+
+pub fn merge_files(files:Vec<&str>) -> io::Result<()> {
+    merge_json_objects(&files, "merged.json")?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use std::error::Error;
+use std::{error::Error, fs};
 use reqwest::blocking::Client;
 
 mod constantes; 
-
+mod fusion;
 fn main() -> Result<(), Box<dyn Error>> {
     let base_url = "https://api.brawlstars.com/v1/players/%23";
     
     for player in constantes::PLAYER.iter(){
-        get_player_info(player, base_url)?;
+        create_files(&player);
+        get_player_battlelog(player, base_url)?;
     }
     
     Ok(())
@@ -27,4 +28,51 @@ fn get_player_info(tag: &str, base_url: &str) -> Result<(), Box<dyn Error>> {
     println!("Body: {}", body);
 
     Ok(())
+}
+fn get_player_battlelog(tag: &str, base_url: &str) -> Result<(), Box<dyn Error>> {
+    let full_url = format!("{}{}/battlelog", base_url, tag);  
+    let client = Client::new();
+
+    let response = client
+        .get(&full_url)
+        .header("Authorization", format!("Bearer {}", constantes::TOKEN))
+        .send()?;  
+
+    println!("Status: {}", response.status());
+    let body = response.text()?;  
+    println!("Body: {}", body);
+
+    Ok(())
+}
+
+
+fn create_files(player_tag: &str)->(){
+    let _ = fs::create_dir(format!("#{}",player_tag));
+    let _ = fs::create_dir(format!("#{}/player",player_tag));
+    let _ = fs::create_dir(format!("#{}/battle",player_tag));
+    let _ = fs::create_dir(format!("#{}/club",player_tag));
+
+}
+
+
+
+fn merge_json(dir_path: &str) -> Result<(), std::io::Error> {
+    let mut files = Vec::new();
+
+    if let Ok(entries) = fs::read_dir(dir_path) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                let path = entry.path();
+                if path.is_file() {
+                    if let Some(file_name) = path.to_str() {
+                        files.push(file_name.to_string());
+                    }
+                }
+            }
+        }
+    } else {
+        eprintln!("Error while reading '{}'", dir_path);
+    }
+
+    fusion::merge_files(files.iter().map(|s| s.as_str()).collect())
 }
